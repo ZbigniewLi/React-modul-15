89faaa2571949245e3f8e8a61930848af9024fe5
Zbigniew Liskiewicz <zbigniew.jeremi.l@gmail.com>
2023-11-13 13:02:39 +0100

    add column

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,8 +1,91 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
+import { useState } from 'react';
+//import { useEffect } from 'react';
+import shortid from 'shortid';
+import ColumnForm from '../ColumnForm/ColumnForm';
 
 
 const List = () => {
+
+    const addCard = (newCard, columnId) => {
+        const columnsUpdated = columns.map(column => {
+            if(column.id === columnId)
+                return { ...column, cards: [...column.cards, { id: shortid(), title: newCard.title }]}
+            else
+                return column
+        })
+    
+        setColumns(columnsUpdated);
+    
+    };
+
+    const [columns, setColumns] = useState([
+        {
+            id: 1,
+            title: 'Books',
+            icon: 'book',
+            cards: [
+                { id: 1, title: 'This is Going to Hurt' },
+                { id: 2, title: 'Interpreter of Maladies' }
+            ]
+        },
+        {
+            id: 2,
+            title: 'Movies',
+            icon: 'film',
+            cards: [
+                { id: 1, title: 'Harry Potter' },
+                { id: 2, title: 'Star Wars' }
+            ]
+        },
+        {
+            id: 3,
+            title: 'Games',
+            icon: 'gamepad',
+            cards: [
+                { id: 1, title: 'The Witcher' },
+                { id: 2, title: 'Skyrim' }
+            ]
+        }
+    ]);
+
+    /*const [columns, setColumns] = useState([
+        { id: 1, title: 'Books', icon: 'book' },
+        { id: 2, title: 'Movies', icon: 'film' },
+        { id: 3, title: 'Games', icon: 'gamepad' }
+    ]);*/
+
+
+
+    const addColumn = newColumn => {
+        setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon, cards: [] }]);
+    };
+   
+    /*const addColumn = newColumn => {
+		setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon }]);
+    };
+
+    /*const addColumn = newColumn => {
+		setColumns([...columns, { id: shortid(), title: newColumn.title }]);
+    };
+
+    /*const handleSubmit = e => {
+        e.preventDefault();
+        setColumns([...columns, { id: shortid(), title: value }]);
+        setValue('');
+    }; 
+
+    //const [value, setValue] = useState('');
+
+  	/*useEffect(() => {
+
+        	setTimeout(() => {
+          		setColumns([...columns, { id: 4, title: 'Test column'}]);
+        	}, 2000);
+
+  	}, []); */
+
     return (
         <div className={styles.list}>
             <header className={styles.header}>
@@ -10,13 +93,19 @@ const List = () => {
             </header>
             <p className={styles.description}>Interesting things I want to check out</p>
             <section className={styles.columns}>
-                <Column title="Books" icon="book" />
-                <Column title="Movies" icon="film" />
-                <Column title="Games" icon= "gamepad"/>
+                {columns.map(column => <Column key={column.id} id={column.id} title={column.title} icon={column.icon} cards={column.cards} addCard={addCard} />)}
             </section>
+            <ColumnForm action={addColumn} />
+
         </div>
-      
     );  
+  
 };
 
   export default List;
+
+  /*section className={styles.columns}>
+  <Column title="Books" icon="book" />
+  <Column title="Movies" icon="film" />
+  <Column title="Games" icon= "gamepad"/>
+</section>*/
